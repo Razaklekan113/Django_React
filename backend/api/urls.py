6ac15841761b4bd947c21f8e0c59601852ddccfe
Razaklekan113 <razaklekan11@gmail.com>
2025-02-03 21:57:15 +0100

    adjusted the code

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -7,7 +7,8 @@ urlpatterns = [
     path("token/", TokenObtainPairView.as_view(), name="get_token"),
     path("token/refresh/", TokenRefreshView.as_view(), name="refresh"),
 
-    path("note/", NoteListCreate.as_view(), name="note"),
+    path("note_list/", NoteListCreate.as_view(), name="note-list"),
+    path("note_create/", NoteListCreate.as_view(), name="note-create"),
     path("note/<int:pk>/", NoteDelete.as_view(), name="delete_note"),
     path("note/<int:pk>/update/", NoteUpdate.as_view(), name="update_note")
 ]
