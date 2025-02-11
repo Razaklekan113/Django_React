75005b0c7ec8dc0fe9f6088b2adc75e88f05afe2
Razaklekan113 <razaklekan11@gmail.com>
2025-02-11 18:42:33 +0100

    added a url for the delete func

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     path("note_list/", NoteListCreate.as_view(), name="note-list"),
     path("note_create/", NoteListCreate.as_view(), name="note-create"),
     path("note/<int:pk>/", NoteDelete.as_view(), name="delete_note"),
-    path("note/<int:pk>/update/", NoteUpdate.as_view(), name="update_note")
+    path("note/<int:pk>/update/", NoteUpdate.as_view(), name="update_note"),
+    path("note/<int:pk>/delete/", NoteDelete.as_view(), name="delete"),
 ]
