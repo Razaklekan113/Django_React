40d02f0087800721ccf1d1bc7681300ae8f48b33
Razaklekan113 <razaklekan11@gmail.com>
2025-02-14 23:01:21 +0100

    updated

--- frontend/src/components/protectedRoute.jsx
+++ frontend/src/components/protectedRoute.jsx
@@ -12,6 +12,23 @@ function ProtectedRoute({ children }) {
         auth().catch(() => setIsAuthorized(false))
     }, [])
 
+    const refreshToken = async () => {
+        const refreshToken = localStorage.getItem(REFRESH_TOKEN);
+        try {
+            const res = await api.post("/api/token/refresh/", {
+                refresh: refreshToken,
+            });
+            if (res.status === 200) {
+                localStorage.setItem(ACCESS_TOKEN, res.data.access)
+                setIsAuthorized(true)
+            } else {
+                setIsAuthorized(false)
+            }
+        } catch (error) {
+            console.log(error);
+            setIsAuthorized(false);
+        }
+    };
 
     const auth = async () => {
         const token = localStorage.getItem(ACCESS_TOKEN);
